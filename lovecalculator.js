1726c4aed7ef50a617faf1cf43097708ea0514c8
sarimharoon77 <sarimharoon77@gmail.com>
2025-10-04 15:04:02 +0500

    use of js

---
+++ lovecalculator.js
@@ -0,0 +1,9 @@
+
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+
+if (lovescore === 100) {
+    alert("your love score is " + lovescore + "%" + "you might be a couple made it heaven");
+} else {
+    alert("your love score is " + lovescore)
+}
