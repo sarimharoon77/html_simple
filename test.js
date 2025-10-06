b5d189cdcc9125983eb6dc2af116df273b5a62f2
sarimharoon77 <sarimharoon77@gmail.com>
2025-10-06 10:03:45 +0500

    practice of js

---
+++ test.js
@@ -0,0 +1,12 @@
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "type": "chrome",
+            "request": "launch",
+            "name": "Launch Chrome against localhost",
+            "url": "http://localhost:8080",
+            "webRoot": "${workspaceFolder}"
+        }
+    ]
+}
+alert("hi , welcome to my website")
