bdfbcc2c5d9ea87400fa2d239b74c310b54e16a5
sarimharoon77 <sarimharoon77@gmail.com>
2025-10-13 17:17:17 +0500

    python for school studies

--- test.py
+++ test.py
@@ -4,3 +4,27 @@ if age > 18 :
     print("you are an adult")
     else :
         print("You are a kid")
+        
+        
+         ---
+
+pi = 3.142
+radius = input("enter the radius")
+
+volume = pi*radius*radius
+print("volume")
+
+
+grade = input("enter your grade")
+ 
+ if grade === "A" :
+    print("excellent")
+ elif grade === "B" :
+    print("good job")
+ elif grade === "C" :
+    print("its alright")
+ elif grade === "D" :
+    print("better luck next time")
+ else :
+    print("invalid input")
+ endif 
