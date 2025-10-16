396fea3461f893d0b39bd5182db23f634e10cc33
sarimharoon77 <sarimharoon77@gmail.com>
2025-10-16 13:53:59 +0500

    testing js

--- test.js
+++ test.js
@@ -31,3 +31,8 @@ document.firstElementChild.firstElementChild;
 
 
 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
