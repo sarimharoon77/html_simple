e520af4826a4ca0497749abea6498ae0152ce624
sarimharoon77 <sarimharoon77@gmail.com>
2025-10-25 18:18:12 +0500

    testing jquery

--- test.js
+++ test.js
@@ -1,38 +1,4 @@
-prompt("enter the year")
-function isLeap(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-        return "leap year.";
-    } else {
-        return "not leap year.";
-    }
+$(document).ready(function() {
+$("h1").css("color" , "red");
+});
 
-}
-
-var = [] ;
-
-function is(name) ;
-
-name = "jack" , "jones" , "james"
-
-count = +1
-
-
-
-
-
-
-document.furstElementChild;
-<html lang="en" dir="ltr">
- <head>-</head>
- <body>-</body>
-</html>
-document.firstElementChild.firstElementChild;
-
-
-
-
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
-var randomImageSource = "images/" + randomDiceImage;
